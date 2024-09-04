0e1b5f46ae32016c78e53860114414875a51344c
Chelsy Moraga <chelmorez4.com@gmail.com>
2024-09-04 16:27:53 -0600

    Guardado

---
+++ app/controllers/hash_controller.rb
@@ -0,0 +1,5 @@
+class HashController < ApplicationController
+  def mostrar
+    @datosHash={nombre: "Chelsy", apellido: "Moraga", telefono: "57852392", correo: "chelsea.moraga@gmail.com"}
+  end
+end
